8861b6b47930ebbd189e9e98f6c73a9d7344c2f4
Yusuke Kawanabe <ysk.kwnb.915@gmail.com>
2015-05-27 17:23:54 +0900

    Initial Commit

---
+++ GlanceWordRainbowImageExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GlanceWordRainbowImageExample
+//
+//  Created by Yusuke Kawanabe on 5/27/15.
+//  Copyright (c) 2015 JSK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
